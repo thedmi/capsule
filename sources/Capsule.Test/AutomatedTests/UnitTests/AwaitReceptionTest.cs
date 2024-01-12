0eb4c8dc89772baffbbcc2617cd818aed02828cf
Dani Michel <daniel.michel@geopraevent.ch>
2024-01-12 21:18:17 +0100

    test: Await reception test

---
+++ sources/Capsule.Test/AutomatedTests/UnitTests/AwaitReceptionTest.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Extensions.Hosting;
+using Shouldly;
+
+namespace Capsule.Test.AutomatedTests.UnitTests;
+
+public class AwaitReceptionTest
+{
+    [Test]
+    public async Task Await_reception_returns_when_invocation_was_started()
+    {
+        var runtimeContext = TestRuntime.Create();
+        var hostedService = (BackgroundService)runtimeContext.Host;
+        
+        var tcs = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        await hostedService.StartAsync(CancellationToken.None);
+
+        var methodRunStarted = false;
+
+        var factory =
+            new AwaitReceptionTestSubjectCapsuleFactory(
+                () => new AwaitReceptionTestSubject(tcs.Task, () => { methodRunStarted = true; }), runtimeContext);
+        
+        var sut = factory.CreateCapsule();
+
+        var sutInvocationTask = sut.ExecuteInnerAsync();
+
+        await Task.Delay(100);
+        
+        sutInvocationTask.IsCompleted.ShouldBeTrue();
+        sutInvocationTask.IsCompletedSuccessfully.ShouldBeTrue();
+        methodRunStarted.ShouldBeTrue();
+        
+        var awaitCompletionTask = sut.SucceedAlwaysAsync();
+        await Task.Delay(100);
+        
+        // The second invocation, this time with await completion, should still be pending because the first one hasn't
+        // finished yet
+        awaitCompletionTask.Status.ShouldBe(TaskStatus.WaitingForActivation);
+        
+        // Now let the first invocation finish
+        tcs.SetResult();
+        await Task.Delay(100);
+        
+        awaitCompletionTask.IsCompleted.ShouldBeTrue();
+        awaitCompletionTask.IsCompletedSuccessfully.ShouldBeTrue();
+
+        (await awaitCompletionTask).ShouldBeTrue();
+        
+        await hostedService.StopAsync(CancellationToken.None);
+        await Task.Delay(100);
+        await hostedService.ExecuteTask!;
+    }
+    
+    [Test]
+    public async Task Await_reception_does_not_throw_exception_when_capsule_method_throws()
+    {
+        var runtimeContext = TestRuntime.Create();
+        var hostedService = (BackgroundService)runtimeContext.Host;
+        
+        var tcs = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        
+        var exception = new InvalidOperationException("oh no");
+        
+        await hostedService.StartAsync(CancellationToken.None);
+
+        var factory =
+            new AwaitReceptionTestSubjectCapsuleFactory(
+                () => new AwaitReceptionTestSubject(tcs.Task, () => throw exception), runtimeContext);
+        
+        var sut = factory.CreateCapsule();
+
+        var sutInvocationTask = sut.ExecuteInnerAsync();
+
+        await Task.Delay(100);
+        
+        sutInvocationTask.IsCompleted.ShouldBeTrue();
+        sutInvocationTask.IsCompletedSuccessfully.ShouldBeTrue();
+        
+        // Ensure that the loop is still active and is able to handle a second invocation
+        (await sut.SucceedAlwaysAsync()).ShouldBeTrue();
+        
+        await hostedService.StopAsync(CancellationToken.None);
+        await Task.Delay(100);
+        await hostedService.ExecuteTask!;
+    }
+    
+    [Test]
+    public async Task Await_reception_does_not_throw_exception_when_capsule_method_is_cancelled()
+    {
+        var runtimeContext = TestRuntime.Create();
+        var hostedService = (BackgroundService)runtimeContext.Host;
+        
+        var tcs = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        
+        await hostedService.StartAsync(CancellationToken.None);
+
+        var factory =
+            new AwaitReceptionTestSubjectCapsuleFactory(
+                () => new AwaitReceptionTestSubject(tcs.Task, () => {}), runtimeContext);
+        
+        var sut = factory.CreateCapsule();
+
+        var sutInvocationTask = sut.ExecuteInnerAsync();
+        tcs.SetCanceled();
+
+        await Task.Delay(100);
+        
+        sutInvocationTask.IsCompleted.ShouldBeTrue();
+        sutInvocationTask.IsCompletedSuccessfully.ShouldBeTrue();
+        
+        // Ensure that the loop is still active and is able to handle a second invocation
+        (await sut.SucceedAlwaysAsync()).ShouldBeTrue();
+        
+        await hostedService.StopAsync(CancellationToken.None);
+        await Task.Delay(100);
+        await hostedService.ExecuteTask!;
+    }
+}
---
+++ sources/Capsule.Test/AutomatedTests/UnitTests/AwaitReceptionTestSubject.cs
@@ -0,0 +1,29 @@
+﻿namespace Capsule.Test.AutomatedTests.UnitTests;
+
+[Capsule]
+public class AwaitReceptionTestSubject : ICapsule
+{
+    private readonly Task _innerTask;
+    private readonly Action _preAction;
+
+    public AwaitReceptionTestSubject(Task innerTask, Action preAction)
+    {
+        _innerTask = innerTask;
+        _preAction = preAction;
+    }
+
+    public async Task InitializeAsync() { }
+
+    [Expose(Synchronization = CapsuleSynchronization.AwaitReception)]
+    public async Task ExecuteInnerAsync()
+    {
+        _preAction();
+        await _innerTask;
+    }
+
+    [Expose]
+    public async Task<bool> SucceedAlwaysAsync()
+    {
+        return true;
+    }
+}
