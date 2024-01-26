885c3e925968a0cf6ff6da3c3fd486773d3b7aa0
Dani Michel <daniel.michel@geopraevent.ch>
2024-01-26 11:26:06 +0100

    test: Add in-memory cache sample

---
+++ sources/Capsule.Test/AutomatedTests/CacheSample/CapsuleCacheSample.cs
@@ -0,0 +1,56 @@
+﻿using Capsule.Extensions.DependencyInjection;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Shouldly;
+
+namespace Capsule.Test.AutomatedTests.CacheSample;
+
+public class CapsuleCacheSample
+{
+    [Test]
+    public async Task Use_cache_concurrently()
+    {
+        using var app = ConfigureHost();
+        
+        var cache = app.Services.GetRequiredService<IMemoryCache>();
+
+        var tasks = Enumerable.Range(0, 1000)
+            .Select(i => Task.Run(() => cache.InsertOrUpdateAsync(i, $"content_{i}")))
+            .ToList();
+
+        await Task.WhenAll(tasks);
+
+        foreach (var i in Enumerable.Range(0, 1000))
+        {
+            var content = await cache.GetAsync(i);
+            content.ShouldBe($"content_{i}");
+        }
+    }
+
+    private static IHost ConfigureHost()
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureServices(
+                services =>
+                {
+                    services.AddLogging(
+                        l =>
+                        {
+                            l.AddNUnit();
+                            l.SetMinimumLevel(LogLevel.Debug);
+                        });
+
+                    services.AddCapsuleHost();
+
+                    services.AddSingleton(new MemoryCache(TimeSpan.FromDays(1)));
+
+                    services.AddSingleton<IMemoryCache>(
+                        p => p.GetRequiredService<MemoryCache>()
+                            .Encapsulate(p.GetRequiredService<CapsuleRuntimeContext>()));
+                })
+            .Build();
+    }
+}
---
+++ sources/Capsule.Test/AutomatedTests/CacheSample/MemoryCache.cs
@@ -0,0 +1,36 @@
+﻿using Capsule.Attribution;
+
+namespace Capsule.Test.AutomatedTests.CacheSample;
+
+[Capsule]
+public class MemoryCache(TimeSpan staleAfter)
+{
+    private readonly Dictionary<int, Entry> _cacheEntries = new();
+
+    [Expose]
+    public async Task InsertOrUpdateAsync(int key, string content)
+    {
+        _cacheEntries[key] = new Entry(DateTime.UtcNow, content);
+    }
+
+    [Expose]
+    public async Task<string?> GetAsync(int key)
+    {
+        return _cacheEntries.GetValueOrDefault(key)?.Content;
+    }
+
+    [Expose(Synchronization = CapsuleSynchronization.AwaitEnqueueing)]
+    public async Task RemoveStaleEntriesAsync()
+    {
+        var staleEntryKeys = _cacheEntries.Where(p => p.Value.Timestamp < DateTime.UtcNow - staleAfter)
+            .Select(p => p.Key)
+            .ToList();
+
+        foreach (var key in staleEntryKeys)
+        {
+            _cacheEntries.Remove(key);
+        }
+    }
+
+    private record Entry(DateTime Timestamp, string Content);
+}
